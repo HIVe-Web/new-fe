c487ce82950cd6497c2e34cf2d716e9309c07d7e
oznchex <114240463+oznchex@users.noreply.github.com>
2024-02-26 21:16:11 +0900

    [Feat] refact box

--- src/components/mobilehome/MobActivityTab.jsx
+++ src/components/mobilehome/MobActivityTab.jsx
@@ -92,7 +92,7 @@ export const MobActivityTab = () => {
                     <MobActivityQuestionText>Q.</MobActivityQuestionText>
                     <MobActivityText>정기 세션은 언제 진행되나요?</MobActivityText>
                 </MobActivityQuestionWrapper>
-                <MobDropDownText show={showDropDown1}>정기 세션은 매주 목요일 오후 6시에 인문사회관 D동 308호에서 진행됩니다!</MobDropDownText>
+                <MobDropDownText show={showDropDown1}>정기 세션은 매주 목요일 오후 6시에 인문사회관 D동 308호에서<br/>진행됩니다!<br/><br/></MobDropDownText>
             </MobActivityTextBox>
 
             <MobActivityTextBox expanded={showDropDown2}>
@@ -100,8 +100,8 @@ export const MobActivityTab = () => {
                     <MobActivityQuestionText>Q.</MobActivityQuestionText>
                     <MobActivityText>정기 세션에서는 어떤 활동을 하나요?</MobActivityText>
                 </MobActivityQuestionWrapper>
-                <MobDropDownText show={showDropDown2}>창업 교육 및 강연, 팀별 프로젝트 진행과 피드백 공유, 공모전 및 대회 준비 등 다양한 활동을 진행합니다. <br/>
-                정기 세션 이외에도 피그마 스터디, 코딩 스터디 등 다양한 활동들을 자체적으로 운영하고 있습니다.</MobDropDownText>
+                <MobDropDownText show={showDropDown2}>창업 교육 및 강연, 팀별 프로젝트 진행과 피드백 공유, 공모전 및<br/>대회 준비 등 다양한 활동을 진행합니다. <br/>
+                정기 세션 이외에도 피그마 스터디, 코딩 스터디 등 다양한 활동들을 자체적으로 운영하고 있습니다.<br/><br/></MobDropDownText>
             </MobActivityTextBox>
 
             <MobActivityTextBox expanded={showDropDown3}>
@@ -109,7 +109,7 @@ export const MobActivityTab = () => {
                     <MobActivityQuestionText>Q.</MobActivityQuestionText>
                     <MobActivityText>팀 빌딩은 어떻게 이루어지나요?</MobActivityText>
                 </MobActivityQuestionWrapper>
-                <MobDropDownText show={showDropDown3}>지원하신 서류와 면접 내용을 바탕으로 운영진이 구성할 예정입니다.</MobDropDownText>
+                <MobDropDownText show={showDropDown3}>지원하신 서류와 면접 내용을 바탕으로 운영진이 구성할 예정입니다.<br/><br/></MobDropDownText>
             </MobActivityTextBox>
 
             <MobActivityTextBox expanded={showDropDown4}>
@@ -118,7 +118,7 @@ export const MobActivityTab = () => {
                     <MobActivityText>창업에 대해 몰라도 활동 가능한가요?</MobActivityText>
                 </MobActivityQuestionWrapper>
                 <MobDropDownText show={showDropDown4}>네! 창업에 대한 기초부터 차근차근 배워나갈 수 있도록 정기 세션이 진행될 예정입니다! <br/>
-                추가적으로 창업 경험이 있는 하이브 기존 부원들로부터 많은 정보와 인사이트를 알아가실 수 있답니다!</MobDropDownText>
+                추가적으로 창업 경험이 있는 하이브 기존 부원들로부터 많은 정보와 인사이트를 알아가실 수 있답니다!<br/><br/></MobDropDownText>
             </MobActivityTextBox>
             
         </MobActivityContainer>
