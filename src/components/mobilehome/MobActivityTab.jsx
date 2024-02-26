c7611fd2c9cc22d1c014d62e96da01c37c0c2610
oznchex <114240463+oznchex@users.noreply.github.com>
2024-02-26 21:26:51 +0900

    [Fix] text box 간격

--- src/components/mobilehome/MobActivityTab.jsx
+++ src/components/mobilehome/MobActivityTab.jsx
@@ -27,6 +27,7 @@ const MobActivityQuestionWrapper = styled.div`
 display: flex;
 width: 90vw;
 align-items: center;
+margin-top: 1vw;
 `;
 
 const MobActivityQuestionText = styled.div`
@@ -55,6 +56,7 @@ line-height: 1.5;
 width: 74vw;
 font-size: 2.8vw;
 margin-left: 9.2vw;
+margin-bottom: 1.4vw;
 display: flex;
 opacity: ${props => props.show ? 1 : 0};
 max-height: ${props => props.show ? "30vw" : "0"};
@@ -92,7 +94,7 @@ export const MobActivityTab = () => {
                     <MobActivityQuestionText>Q.</MobActivityQuestionText>
                     <MobActivityText>정기 세션은 언제 진행되나요?</MobActivityText>
                 </MobActivityQuestionWrapper>
-                <MobDropDownText show={showDropDown1}>정기 세션은 매주 목요일 오후 6시에 인문사회관 D동 308호에서<br/>진행됩니다!<br/><br/></MobDropDownText>
+                <MobDropDownText show={showDropDown1}>정기 세션은 매주 목요일 오후 6시에 인문사회관 D동 308호에서<br/>진행됩니다!</MobDropDownText>
             </MobActivityTextBox>
 
             <MobActivityTextBox expanded={showDropDown2}>
@@ -101,7 +103,7 @@ export const MobActivityTab = () => {
                     <MobActivityText>정기 세션에서는 어떤 활동을 하나요?</MobActivityText>
                 </MobActivityQuestionWrapper>
                 <MobDropDownText show={showDropDown2}>창업 교육 및 강연, 팀별 프로젝트 진행과 피드백 공유, 공모전 및<br/>대회 준비 등 다양한 활동을 진행합니다. <br/>
-                정기 세션 이외에도 피그마 스터디, 코딩 스터디 등 다양한 활동들을 자체적으로 운영하고 있습니다.<br/><br/></MobDropDownText>
+                정기 세션 이외에도 피그마 스터디, 코딩 스터디 등 다양한 활동들을 자체적으로 운영하고 있습니다.</MobDropDownText>
             </MobActivityTextBox>
 
             <MobActivityTextBox expanded={showDropDown3}>
@@ -109,7 +111,7 @@ export const MobActivityTab = () => {
                     <MobActivityQuestionText>Q.</MobActivityQuestionText>
                     <MobActivityText>팀 빌딩은 어떻게 이루어지나요?</MobActivityText>
                 </MobActivityQuestionWrapper>
-                <MobDropDownText show={showDropDown3}>지원하신 서류와 면접 내용을 바탕으로 운영진이 구성할 예정입니다.<br/><br/></MobDropDownText>
+                <MobDropDownText show={showDropDown3}>지원하신 서류와 면접 내용을 바탕으로 운영진이 구성할 예정입니다.</MobDropDownText>
             </MobActivityTextBox>
 
             <MobActivityTextBox expanded={showDropDown4}>
@@ -118,7 +120,7 @@ export const MobActivityTab = () => {
                     <MobActivityText>창업에 대해 몰라도 활동 가능한가요?</MobActivityText>
                 </MobActivityQuestionWrapper>
                 <MobDropDownText show={showDropDown4}>네! 창업에 대한 기초부터 차근차근 배워나갈 수 있도록 정기 세션이 진행될 예정입니다! <br/>
-                추가적으로 창업 경험이 있는 하이브 기존 부원들로부터 많은 정보와 인사이트를 알아가실 수 있답니다!<br/><br/></MobDropDownText>
+                추가적으로 창업 경험이 있는 하이브 기존 부원들로부터 많은 정보와 인사이트를 알아가실 수 있답니다!</MobDropDownText>
             </MobActivityTextBox>
             
         </MobActivityContainer>
--- src/components/mobilehome/MobApplyTab.jsx
+++ src/components/mobilehome/MobApplyTab.jsx
@@ -27,6 +27,7 @@ const MobApplyQuestionWrapper = styled.div`
     display: flex;
     width: 90vw;
     align-items: center;
+    margin-top: 1vw;
 `;
 
 const MobApplyQuestionText = styled.div`
@@ -55,6 +56,7 @@ line-height: 1.5;
 width: 74vw;
 font-size: 2.8vw;
 margin-left: 9.2vw;
+margin-bottom: 1.4vw;
 display: flex;
 opacity: ${props => props.show ? 1 : 0};
 max-height: ${props => props.show ? "30vw" : "0"};
@@ -93,7 +95,7 @@ export const MobApplyTab = () => {
                     <MobApplyText>프로젝트나 협업 경험이 없어도 지원 가능한가요?</MobApplyText>
                 </MobApplyQuestionWrapper>
                 <MobDropDownText show={showDropDown1}>네, 창업에 대한 의지만 있다면 누구나 가능합니다!<br/>
-                하이브는 열정을 가지고 진심어린 마음으로 창업 경험을 쌓고 싶은 분들을 기다리고 있습니다!<br/><br/>
+                하이브는 열정을 가지고 진심어린 마음으로 창업 경험을 쌓고 싶은 분들을 기다리고 있습니다!
                 </MobDropDownText>
             </MobApplyTextBox>
 
@@ -104,7 +106,7 @@ export const MobApplyTab = () => {
                 </MobApplyQuestionWrapper>
                 <MobDropDownText show={showDropDown2}>포트폴리오는 필수가 아니지만 제출 가능합니다! <br/>
                 포트폴리오가 있으신 분들은 능력과 경험을 추가적으로 어필하실 수 있답니다!
-                포트폴리오가 없으신 분들은 서류에서 창업에 대한 의지와 하이브 활동에 대한 열정을 중점으로 지원자님을 어필해보세요!<br/><br/>
+                포트폴리오가 없으신 분들은 서류에서 창업에 대한 의지와 하이브 활동에 대한 열정을 중점으로 지원자님을 어필해보세요!<br/>
                 </MobDropDownText>
             </MobApplyTextBox>
 
@@ -113,7 +115,7 @@ export const MobApplyTab = () => {
                     <MobApplyQuestionText>Q.</MobApplyQuestionText>
                     <MobApplyText>면접은 어떤 방식으로 진행되나요?</MobApplyText>
                 </MobApplyQuestionWrapper>
-                <MobDropDownText show={showDropDown3}>면접은 대면으로 진행되며 작성해주신 서류 항목 중심으로 <br/>질의응답이 이루어집니다!<br/><br/></MobDropDownText>
+                <MobDropDownText show={showDropDown3}>면접은 대면으로 진행되며 작성해주신 서류 항목 중심으로 <br/>질의응답이 이루어집니다!<br/></MobDropDownText>
             </MobApplyTextBox>
 
             <MobApplyTextBox expanded={showDropDown4}>
@@ -121,7 +123,7 @@ export const MobApplyTab = () => {
                     <MobApplyQuestionText>Q.</MobApplyQuestionText>
                     <MobApplyText>지원 결과는 어떻게 확인할 수 있나요?</MobApplyText>
                 </MobApplyQuestionWrapper>
-                <MobDropDownText show={showDropDown4}>서류, 면접 결과 모두 기재해주신 연락처로 합불 여부에 대해 연락을 드릴 예정입니다!<br/><br/></MobDropDownText>
+                <MobDropDownText show={showDropDown4}>서류, 면접 결과 모두 기재해주신 연락처로 합불 여부에 대해 연락을 드릴 예정입니다!<br/></MobDropDownText>
             </MobApplyTextBox>
         </MobApplyContainer>
     );
