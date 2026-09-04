7c4a153967a23724ad5a4596f73726671624e324
juyungko1001-glitch <juyungko1001@gmail.com>
2026-09-04 18:11:32 +0900

    Update calendar deadline to September 8

--- src/components/mobilehome/MobCalendar.jsx
+++ src/components/mobilehome/MobCalendar.jsx
@@ -71,7 +71,7 @@ export const MobCalendar = () => {
     <MobHeadContainer>
       <MobCalendarContainer>
         <MobCalendarText>모집 마감 기한</MobCalendarText>
-        <MobCalendarDateText>3월 14일 23시 59분까지</MobCalendarDateText>
+        <MobCalendarDateText>9월 8일 23시 59분까지</MobCalendarDateText>
         <MobCalendarImage data={calendarImage} type="" />
       </MobCalendarContainer>
       <MobHistoryTitle> 
